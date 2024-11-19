cee4b27ad2ce555ae31bc1b4cca98076b394b1fd
Jean-Lionel <nijeanlionel@gmailcom>
2024-11-19 07:35:10 +0200

    add list icons

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <app-header/>
+        <router-view></router-view>
+        <app-side-bar/>
+    </div>
+</template>
+
+<script>
+import AppHeader from './AppHeader.vue'
+import AppSideBar from './AppSideBar.vue'
+export default {
+  components: { AppHeader, AppSideBar },
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
